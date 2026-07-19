a60867beaeb1f08e3978dc66c728aa503b90dda3
inscope-labs <afterdawn.service@gmail.com>
2026-07-19 13:13:32 -0400

    build: add androidx.constraintlayout dependency
    Added the ConstraintLayout library to support UI layout inflation.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -96,6 +96,7 @@ dependencies {
   implementation(libs.androidx.core.ktx)
   implementation(libs.androidx.webkit)
   implementation(libs.androidx.appcompat)
+  implementation(libs.androidx.constraintlayout)
   implementation(libs.androidx.activity.compose)
   implementation(platform(libs.androidx.compose.bom))
   implementation(libs.androidx.compose.ui)
